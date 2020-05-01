7acf05427ad292f06d5e3b7144844307fbf40dff
Nadja Brandys <nadiusza@gmail.com>
2020-05-01 14:33:30 +0200

    First commit ex.1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Todo from "./components/Todo";
+
+
+function App () {
+    
+    return(
+    
+    <div>
+<Todo />
+<Todo />
+<Todo />
+<Todo />
+   </div>
+    )
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import style from "../style.scss";
+
+function Todo() {
+
+  return (
+    <div className={style.mainDiv}>
+      <input type="checkbox" />
+        <p className={style.p}>placeholder</p>
+     
+    </div>
+  )
+}
+export default Todo;
