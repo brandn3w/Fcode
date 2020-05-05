529fc0d8f09164c081393409bb4da67efe24fb77
Nadja Brandys <nadiusza@gmail.com>
2020-05-05 17:32:26 +0200

    practicing setState

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import React from "react"
 
 class App extends React.Component {
     constructor() {
-        super()
-        this.state = {
-            count:0
-        }
-        this.handleClick = this.handleClick.bind(this)
+super()
+this.state={
+    count:0
+}
+this.handleClick = this.handleClick.bind(this)
     }
-    handleClick(){
-        this.setState(prevState=>{
-return{
+handleClick(){
+this.setState(prevState=> {
+    return{
     count: prevState.count+1
 }
-        })
-    }
+})
+}
     render(){
         return(
             <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.handleClick }>Change</button>
-            </div>
+<h1>{this.state.count}</h1>
+<button onClick ={this.handleClick}>Click me</button>
+</div>
         )
     }
 }
