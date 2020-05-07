13a1a9a4d6398213de10c3d00b3ba871c2098497
Nadja Brandys <nadiusza@gmail.com>
2020-05-08 01:12:51 +0200

    conditional rendering practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 
 import React from "react";
-import Todo from "./components/Todo";
 
 
-function App () {
-    
-    return(
-    
-    <div>
-<Todo />
-<Todo />
-<Todo />
-<Todo />
-   </div>
-    )
+
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+
+    this.handleClick=this.handleClick.bind(this)
+    }
+handleClick(){
+    this.setState(prevState =>{
+        return{
+isLoggedIn: !prevState.isLoggedIn
+        }
+    })
 }
-
+    render() {
+let buttonText = this.state.isLoggedIn ? "log out" : "log in"
+let buttonDisplay = this.state.isLoggedIn ? "logged in" : "logged out"
+        return (
+            <div>
+        <button onClick={this.handleClick}>{buttonText}</button>
+        <h1>{displayText}</h1>
+        </div>
+    )
+    }
+}
 export default App;
