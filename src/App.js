37860a64238f1842ada02e75238e8ef32adb473f
Nadja Brandys <nadiusza@gmail.com>
2020-05-10 00:20:11 +0200

    firms first part

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
 
 import React from "react";
-import Todo from "./components/Todo";
 
-import todoData from "./components/todoData";
 class App extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            todos: todoData
+            firstName: "",
+            lastName: "",
+            isfriendly: true
+            gender:""
         }
         this.handleChange = this.handleChange.bind(this)
     }
-    handleChange(id) {
-        this.setState(prevState => {
-            const updatedTodo = prevState.todos.map(todo =>{
-            if (todo.id = id) {
-                todo.completed = !todo.completed
-            }
-                return todo
-
-            })
-            return {
-                todos: updatedTodo
-            }
-        })
+    handleChange(event) {
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
     }
-
     render() {
-        const todoItems = this.state.todos.map(item => <Todo key={item.id} item={item}
-            handleChange={this.handleChange} /> )
         return (
+            <form>
+                <input type="text" value={this.state.firstname} name="firstname" placeHolder="first name" onChange={this.handleChange} />
+                <br />
+                <input type="text" value={this.state.lastname} name="lastName" placeHolder="last name" onChange={this.handleChange} />
 
-            <div>
-                {todoItems}
-            </div>
 
-        )
-    }
+                <textarea value={text}
+                    onChange={this.handleChange
+                        />
+                        <br />
+                        <label>
+                            <input type="checkbox"
+                                name="isFriendly"
+                                checked={this.state.isFriendly}
+                                onChange={this.handleChange}
+                            />
+                        </label>
+
+                        <label>
+                            <input type="radio"
+                            name="gender"
+                            value="female"
+                            checked={this.state.gender==="female"}
+                            onChange={this.handleChange}
+                            /> Female
+                        </label>
+
+                         <label>
+                         <input type="radio"
+                         name="gender"
+                         value="male"
+                         checked={this.state.gender==="male"}
+                         onChange={this.handleChange}
+                         /> male
+                     </label>
+
+                     <select>
+                         <option></option>
+                         <option></option>
+                         <option></option>
+                     </select>
+                        <h1>{this.state.firstName} {this.state.lastName}</h1>
+        </form>
+    )
 }
-export default App;
+
+export default App
